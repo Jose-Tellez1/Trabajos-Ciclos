1daeab97465ddf9059423f016c1204197aff4359
Jose-Tellez1 <1232100587@univermilenium.edu.mx>
2024-06-17 23:06:48 -0600

    Se agrega el proyecto

---
+++ Tabla del 5.js
@@ -0,0 +1,4 @@
+let numero = 5;
+for(let a = 1; a < 11; a++){
+    console.log(numero + "*" + a + "=" + numero * a)
+}
---
+++ Tablas de 1 al 10.js
@@ -0,0 +1,5 @@
+for(let num = 1; num < 11; num++){
+    for(let z = 1; z <= 10; z++){
+        console.log(num + "*" + z + "=" + num * z)
+    }
+}
